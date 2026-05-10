3c18712fb8ee8b5c3a5122ade163535ab10d144c
Rashmi0195 <rashmirrao1595@gmail.com>
2026-05-10 12:20:00 +0000

    container config done

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+// Variables
+let myName = "John";
+const age = 30;
+console.log(typeof myName);
+
+const accountID = 12345;
+let isActive = true;
+let accountEmail = "john@example.com";
+accountCity = "New York"; // Implicit global variable (not recommended)
+let accountState
+console.log(accountCity);
+console.log(typeof accountID);
+console.log(typeof isActive);
+console.log(typeof accountEmail);
+console.log(typeof accountCity);
+console.log(typeof accountState);
