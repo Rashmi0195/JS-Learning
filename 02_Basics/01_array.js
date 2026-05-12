78c7bcd6456f201bcb6e63bd3a302885f3978ead
Rashmi0195 <rashmirrao1595@gmail.com>
2026-05-12 15:26:25 +0000

    Arrays

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,32 @@
+const myArr = new Array(1, 2, 3, 4, 5);
+console.log(myArr);
+
+const myArr2 = [1, 2, 3, 4, 5];
+console.log(myArr2);
+myArr2.push(6);
+console.log(myArr2);
+
+myArr2.pop();
+console.log(myArr2);
+
+const myArr3 = new Array(5);
+console.log(myArr3);
+
+const myArr4 = new Array(1, 2, 3, 4, 5);
+console.log(myArr4.length);
+
+myArr.unshift(9);
+console.log(myArr);
+
+myArr.shift();
+console.log(myArr);
+
+myArr.splice(1,3);
+console.log(myArr);
+
+myArr.slice(2, 1);
+console.log(myArr);
+
+const myArr5 = [1, 2, 3, 4, 5];
+const myArr6 = myArr5.slice(0, 3);
+console.log(myArr6);
