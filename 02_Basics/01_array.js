49e6477980852592f316468f6851acc1eb699c7f
Rashmi0195 <rashmirrao1595@gmail.com>
2026-05-13 15:18:42 +0000

    arrays modified

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -30,3 +30,16 @@ console.log(myArr);
 const myArr5 = [1, 2, 3, 4, 5];
 const myArr6 = myArr5.slice(0, 3);
 console.log(myArr6);
+
+// concat and push
+const fruit1 = ['apple', 'banana'];
+const fruit2 = ['orange', 'grape'];
+const allFruit = fruit1.concat(fruit2);
+console.log(allFruit);
+
+fruit1.push(...fruit2);// spread operator, spread the elements of fruit2 into the push method
+console.log(fruit1);
+
+const originalArray = [1, 2, 3,[4,5,6],7,[8,9,[10,11]]];
+const flatArray = originalArray.flat(Infinity);
+console.log(flatArray);
